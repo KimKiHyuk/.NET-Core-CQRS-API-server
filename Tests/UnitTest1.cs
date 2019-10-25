d60c84eb49f662c8aa7edb46c04c98d09040f0a5
key Kim <keyhyuk.kim@gmail.com>
2019-10-25 13:10:46 +0900

    Add Test component using xunit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests // project hierchy is bad. move this folder to outside of RestApi
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
